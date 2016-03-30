81db4f8b611e509813187e8d95c834d746ad5221
Nikoloz <nikoloz.astamidze@gmail.com>
2016-03-29 18:34:50 -0700

    Project Birth

---
+++ Unity_Game/assets/Scripts/Orbit.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[ExecuteInEditMode]
+public class Orbit : MonoBehaviour {
+
+	float timeCounter=0f;
+
+	public float speed = 1f;
+	public float l1 = 1f;
+	public float l2 = 1f;
+	public GameObject cObject;
+
+
+	//var playerPos:Vector3 = cObject.transform.position;
+
+	// Use this for initialization
+	void Start () {
+		//speed = 1;
+		//l1 = 1;
+		//l2 = 1;
+	}
+
+	// Update is called once per frame
+	void Update () {
+		timeCounter += Time.deltaTime*speed;
+
+		float x = Mathf.Cos (timeCounter)*l1;
+		float y = 0;
+		float z = Mathf.Sin (timeCounter)*l2;
+
+		transform.position = new Vector3 (x, y, z);
+		transform.position = transform.position;
+			//* cObject.transform.positoin;
+	}
+}
